ab35eaac3f9a8d8384d6a0118b4fc17611f3265a
mitch-mooney <mitchellgmooney@gmail.com>
2026-01-12 17:43:05 +1100

    auto-claude: subtask-4-1 - Add selectedAnnotation to annotation store

--- src/store/annotationStore.ts
+++ src/store/annotationStore.ts
@@ -14,21 +14,25 @@ export interface Annotation {
 
 interface AnnotationState {
   annotations: Annotation[];
+  selectedAnnotationId: string | null;
   selectedTool: AnnotationType | null;
   selectedColor: string;
   thickness: number;
-  
+
   // Actions
   addAnnotation: (annotation: Omit<Annotation, 'id' | 'createdAt'>) => void;
   removeAnnotation: (id: string) => void;
   clearAnnotations: () => void;
+  selectAnnotation: (annotationId: string | null) => void;
   setSelectedTool: (tool: AnnotationType | null) => void;
   setSelectedColor: (color: string) => void;
   setThickness: (thickness: number) => void;
+  getAnnotation: (annotationId: string) => Annotation | undefined;
 }
 
-export const useAnnotationStore = create<AnnotationState>((set) => ({
+export const useAnnotationStore = create<AnnotationState>((set, get) => ({
   annotations: [],
+  selectedAnnotationId: null,
   selectedTool: null,
   selectedColor: '#ffff00', // Yellow default
   thickness: 2,
@@ -47,13 +51,18 @@ export const useAnnotationStore = create<AnnotationState>((set) => ({
   removeAnnotation: (id) => {
     set((state) => ({
       annotations: state.annotations.filter((a) => a.id !== id),
+      selectedAnnotationId: state.selectedAnnotationId === id ? null : state.selectedAnnotationId,
     }));
   },
   
   clearAnnotations: () => {
-    set({ annotations: [] });
+    set({ annotations: [], selectedAnnotationId: null });
   },
-  
+
+  selectAnnotation: (annotationId) => {
+    set({ selectedAnnotationId: annotationId });
+  },
+
   setSelectedTool: (tool) => {
     set({ selectedTool: tool });
   },
@@ -64,5 +73,9 @@ export const useAnnotationStore = create<AnnotationState>((set) => ({
   
   setThickness: (thickness) => {
     set({ thickness });
+  },
+
+  getAnnotation: (annotationId) => {
+    return get().annotations.find((a) => a.id === annotationId);
   },
 }));
